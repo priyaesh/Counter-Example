eb108969be6bd68752713a4f3387bf39ecf80176
hemapriya <hemapriyasampath@gmail.com>
2022-01-25 10:14:42 -0500

    adding components

---
+++ src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: 4,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return <ul>{this.state.tags.map((tag = <li key={tag}>{tag}</li>))}</ul>;
+  }
+
+  render() {
+    let classes = this.getBadgeClasses();
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
+
+export default Counter;
